6c7759235cbe77658d0a7027c17deb1c346aafdc
ArthurCTU <marthur27@ymail.com>
2017-03-29 16:43:40 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,21 @@
+
+    <div class="container">
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <center><p> CSI2131 &copy; DATABASES <?php $day=date("d");
+$month=date("m");
+$year=date("y");
+ echo $day."-".$month."-".$year; ?></p></center>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+</body>
+
+</html>
